b523ec7abbbcd86b33e5ba78008644bd91539723
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-05-08 19:11:10 +0530

    Update JumpGame.cpp

--- Array/Medium/JumpGame.cpp
+++ Array/Medium/JumpGame.cpp
@@ -1,4 +1,31 @@
 class Solution {
+    vector<int> memo;
+public:
+    bool canJump(vector<int>& nums) {
+      memo.assign(nums.size(),-1);
+      return solve(0,nums);
+        
+    }
+    
+    bool solve(int i,vector<int> nums){
+        
+        if(i==nums.size()-1) return true;
+        if(memo[i]!=-1) return memo[i];
+        int reachable=i+nums[i];
+        
+        for(int k=i+1;k<=reachable;k++){
+            
+            if(solve(k,nums)){
+                return memo[i]=true;
+            }
+            
+        }
+        
+        return memo[i]=false;
+    }
+};
+
+class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
