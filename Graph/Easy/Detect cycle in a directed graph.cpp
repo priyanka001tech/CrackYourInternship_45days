310b6dd0de977d16fb179a10d3632457b650b3da
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-05-11 20:01:27 +0530

    Create Detect cycle in a directed graph.cpp

---
+++ Graph/Easy/Detect cycle in a directed graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool cycleUtil(int src, vector<int> adj[], vector<int>& vis, vector<int>& st){
+        st[src]=1;
+        if(!vis[src]){
+            vis[src]=1;
+            for(auto i:adj[src]){
+                if(st[i]) return true;
+                if(!vis[i] && cycleUtil(i, adj, vis, st)){
+                    return true;
+                }
+            }
+        }
+        st[src]=0;
+        return false;
+        
+    }
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int> vis(V, 0);
+        vector<int> st(V, 0);
+        bool c=false;
+        for(int i=0;i<V;i++){
+            if(!vis[i] && cycleUtil(i, adj, vis, st)){
+                c=true;
+            }
+        }
+        if(c) return true;
+        else return false;
+    }
+};
