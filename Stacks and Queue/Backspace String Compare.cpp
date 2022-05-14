35570d263cfcd2bd8916522b30248a045deb12f2
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-05-14 22:57:24 +0530

    Create Backspace String Compare.cpp

---
+++ Stacks and Queue/Backspace String Compare.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int j=0,k=0;
+        // for 1st string
+        for(int i=0;i<s.size();i++)
+        {
+           if(s[i]=='#')
+           {
+                j--;
+                j=max(0,j); // j will never be -ve
+           }
+           else
+           {
+               s[j]=s[i];
+               j++;
+           }
+        }
+        cout << s << endl;
+        // for 2nd string
+        for(int i=0;i<t.size();i++)
+        {
+           if(t[i]=='#')
+           {
+                k--;
+                k=max(0,k); // k will never be -ve
+           }
+           else
+           {
+               t[k]=t[i];
+               k++;
+           }
+        }
+        cout << t << endl;
+        if(j!=k) return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i]) return false;
+            }
+            return true;
+        } 
+    }
+};
