528fe35a5c600ea2f28cf7bba352af36d763ba24
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-05-17 18:55:24 +0530

    Update The kth Factor of n.cpp

--- Mathematical Problem/Medium/The kth Factor of n.cpp
+++ Mathematical Problem/Medium/The kth Factor of n.cpp
@@ -9,3 +9,26 @@ public:
         return factors[k-1];
     }
 };
+
+
+// Another Approach
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int>v;
+        v.push_back(1);
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0)
+            {
+                v.push_back(i);
+                if(i*i!=n)
+                    v.push_back(n/i);
+            }
+        }
+        v.push_back(n);
+        sort(v.begin(),v.end());
+        if(k>v.size())
+            return -1;
+        return v[k-1];
+    }
+};
