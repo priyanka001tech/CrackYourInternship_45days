ea06ae14bdc909939de198c5f147d15e428c5fff
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-05-29 00:41:33 +0530

    Update Cheapest Flights Within K Stops.cpp

--- Graph/Medium/Cheapest Flights Within K Stops.cpp
+++ Graph/Medium/Cheapest Flights Within K Stops.cpp
@@ -1,3 +1,6 @@
+// O(n * m * logn)
+// O(n)
+
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
@@ -18,5 +21,31 @@ public:
             }
         }
         return -1;
+    }
+};
+
+
+// BELLMON FORD 
+// O(n*m)
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& edges, int src, int dst, int k) {
+        vector<int> dist(n, INT_MAX);
+        dist[src]=0;
+        for(int i=0; i<=k; i++) {
+            vector<int> temp(n);
+            copy(dist.begin(), dist.end(), temp.begin());
+            for(auto e : edges) {
+                if(dist[e[0]]==INT_MAX)
+                    continue;
+                if(temp[e[1]]>dist[e[0]]+e[2])
+                    temp[e[1]]=dist[e[0]]+e[2];
+            }
+            copy(temp.begin(), temp.end(), dist.begin());
+        }
+        if(dist[dst]==INT_MAX)
+            return -1;
+        return dist[dst];
     }
 };
