7ddde69cc4811eaed23526cbc84c60ec94209ac3
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-05-30 14:28:20 +0530

    Update Product of Array except itself.cpp

--- Sorting and searching/Medium/Product of Array except itself.cpp
+++ Sorting and searching/Medium/Product of Array except itself.cpp
@@ -5,6 +5,8 @@ using namespace std;
 for a given array arr[] of size n */
 
 // USING PREFIX AND SUFFIX ARRAY...
+//  TC O(N) 
+//  SC O(N0
 
 void productArray(int arr[], int n)
 {
@@ -63,3 +65,80 @@ int main()
 	cout << "The product array is: \n";
 	productArray(arr, n);
 }
+
+// O(N)
+// SC O(1)
+// C++ program for the above approach
+#include <iostream>
+using namespace std;
+
+long* productExceptSelf(int a[], int n)
+{
+	long prod = 1;
+	long flag = 0;
+
+	// product of all elements
+	for (int i = 0; i < n; i++) {
+
+		// counting number of elements
+		// which have value
+		// 0
+		if (a[i] == 0)
+			flag++;
+		else
+			prod *= a[i];
+	}
+
+	// creating a new array of size n
+	long* arr = new long[n];
+
+	for (int i = 0; i < n; i++) {
+
+		// if number of elements in
+		// array with value 0
+		// is more than 1 than each
+		// value in new array
+		// will be equal to 0
+		if (flag > 1) {
+			arr[i] = 0;
+		}
+
+		// if no element having value
+		// 0 than we will
+		// insert product/a[i] in new array
+		else if (flag == 0)
+			arr[i] = (prod / a[i]);
+
+		// if 1 element of array having
+		// value 0 than all
+		// the elements except that index
+		// value , will be
+		// equal to 0
+		else if (flag == 1 && a[i] != 0) {
+			arr[i] = 0;
+		}
+
+		// if(flag == 1 && a[i] == 0)
+		else
+			arr[i] = prod;
+	}
+	return arr;
+}
+
+// Driver Code
+int main()
+{
+	int n = 5;
+	int array[] = { 10, 3, 5, 6, 2 };
+
+	long* ans;
+	ans = productExceptSelf(array, n);
+
+	for (int i = 0; i < n; i++) {
+		cout << ans[i] << " ";
+	}
+	// cout<<"GFG!";
+	return 0;
+}
+
+// This code is contributed by RohitOberoi.
