bdfc2a78513989b3643a22446cd0ed98c0562977
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-06-07 16:10:52 +0530

    Update Rotate Image.cpp

--- Matrix/Rotate Image.cpp
+++ Matrix/Rotate Image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-      //Transpose
+        //Transpose
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 swap(matrix[i][j],matrix[j][i]); 
