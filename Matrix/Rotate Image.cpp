f970bc662248e74e188a8c6614fd60d44f183908
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-06-07 16:10:43 +0530

    Update Rotate Image.cpp

--- Matrix/Rotate Image.cpp
+++ Matrix/Rotate Image.cpp
@@ -5,7 +5,7 @@ public:
       //Transpose
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j],matrix[j][i]); 
             }
         }
       //Reverse Matrix
