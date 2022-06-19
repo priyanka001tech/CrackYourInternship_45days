fd69d9ac26fe710286991e4917fec0b09f4013f0
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-06-20 00:53:40 +0530

    Create Binary Tree Inorder Traversal.cpp

---
+++ Tree/Easy/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void findAns(TreeNode* root,vector<int>& ans){
+        if(root==NULL){
+            return;
+        }
+        findAns(root->left,ans);
+        ans.push_back(root->val);
+        findAns(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        findAns(root,ans);
+        return ans;
+    }
+};
