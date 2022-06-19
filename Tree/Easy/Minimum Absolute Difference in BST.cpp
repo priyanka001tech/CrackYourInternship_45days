a1ee0514d111fce6b3a3822910cf242d4f45172f
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-06-20 01:02:10 +0530

    Create Minimum Absolute Difference in BST.cpp

---
+++ Tree/Easy/Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int min_dif = INT_MAX, val = -1;    
+    int getMinimumDifference(TreeNode* root) {
+        if (root->left != NULL) 
+            getMinimumDifference(root->left);
+        if (val >= 0) 
+            min_dif = min(min_dif, root->val - val);
+        val = root->val;
+        if (root->right != NULL) 
+            getMinimumDifference(root->right);
+        return min_dif;
+    }
+};
