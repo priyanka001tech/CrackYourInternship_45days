debc8672b8f91cf0fa3d24b6af31be4fa0307623
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-06-22 23:33:06 +0530

    Create Balanced Binary Tree.cpp

---
+++ Tree/Easy/Balanced Binary Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findAns(TreeNode* root){
+        if(root==NULL) return 0;
+        
+        int l=findAns(root->left);
+        if(l==-1) return -1;
+        
+        int r=findAns(root->right);
+        if(r==-1) return -1;
+        
+        if(abs(l-r)>1) return -1;
+        
+        return max(l,r)+1;
+        
+    }
+    bool isBalanced(TreeNode* root) {
+        if(findAns(root)==-1) return false;
+        else return true;
+    }
+};B
