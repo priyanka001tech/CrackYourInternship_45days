1faddb080d6becdd5cc6048cb9ce9fa170c2b9b6
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-06-24 00:48:40 +0530

    Update Balanced Binary Tree.cpp

--- Tree/Easy/Balanced Binary Tree.cpp
+++ Tree/Easy/Balanced Binary Tree.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findAns(TreeNode* root){
-        if(root==NULL) return 0;
+        if(root==NULL) return 0; 
          
         int l=findAns(root->left); 
         if(l==-1) return -1;
